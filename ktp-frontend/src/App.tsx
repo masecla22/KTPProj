f8cba35f2002e88f2f4f6d2e124a51e7fe76dd22
masecla22 <supermasecla22@gmail.com>
2024-12-06 16:01:49 +0100

    Update App.tsx

--- ktp-frontend/src/App.tsx
+++ ktp-frontend/src/App.tsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import QuizPage from "./pages/quiz-page"
+import ResultsPage from "./pages/result-page"
 
 function App() {
-  return <QuizPage />
+  // return <QuizPage />
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<QuizPage />} />
+      <Route path="/result" element={<ResultsPage />} />
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
